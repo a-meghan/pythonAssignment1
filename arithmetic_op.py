7a7c60291b97732070793256562edc3cb47863fe
a-meghan <a.meghanramsey@gmail.com>
2024-09-17 22:43:16 -0400

    added second task to arithmetic_op

--- arithmetic_op.py
+++ arithmetic_op.py
@@ -1,4 +1,14 @@
 #Task 1
+
 bread, peanut_butter, jelly = 3.15, 4.99, 5.99
 sum_total = bread + peanut_butter + jelly
 print("The price of these three items is ", sum_total)
+
+
+#Task 2
+
+#Formula- I (initial amount) * (1 + r(annual interest rate) / 100)
+I = 10000
+r = 7
+after_one_year = I * (1 + r / 100)
+print("The amount after one year comes out to ",after_one_year)
