2bb1e3aa9c70bd94fc4a24e4998166751d39d563
a-meghan <a.meghanramsey@gmail.com>
2024-09-17 22:21:22 -0400

    first commit

---
+++ arithmetic_op.py
@@ -0,0 +1,4 @@
+#Task 1
+bread, peanut_butter, jelly = 3.15, 4.99, 5.99
+sum_total = bread + peanut_butter + jelly
+print("The price of these three items is ", sum_total)
---
+++ dance_moves.py
@@ -0,0 +1,19 @@
+#Task 1
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+#Task 2
+moodToday = input("How are you feeling today? ")
+
+if moodToday.title() == 'Happy':
+    print("That's great to hear!")
+elif moodToday.title() == 'Sad':
+    print("I hope your day gets better! ")
+else:
+    print("Sorry! I don't understand, please try again.")
